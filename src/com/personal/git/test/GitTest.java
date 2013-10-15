d38147e71dfdd1e840d9fd43b83024ba2021e054
freemanzhangjing <461785180@qq.com>
2013-10-15 16:01:18 +0800

    first commit ==================== 2013-10-15 16:01:14

---
+++ src/com/personal/git/test/GitTest.java
@@ -0,0 +1,21 @@
+/**    
+ * @Title: GitTest.java 
+ * @Package com.personal.git.test 
+ * @author zhangjing  
+ * @date 2013-10-15 下午03:17:08 
+ * @version V1.0    
+ */
+package com.personal.git.test;
+
+/**   
+ * This class is used for :   
+ *         
+ * @author zhangjing  
+ * @version 1.0
+ * @date 2013-10-15 下午03:17:08   
+ */
+public class GitTest {
+	public void myFirstMethod(){
+		
+	}
+}
